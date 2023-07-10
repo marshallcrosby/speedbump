5970c949e6772e7b1471da69c4b66820594a539d
Marshall Crosby <marshall@blendinteractive.com>
2023-07-10 13:25:05 -0500

    Convert to vanilla javascript
    Remove jQuery as a dependency

--- dist/assets/js/speedbump.js
+++ dist/assets/js/speedbump.js
@@ -1,230 +1,214 @@
 /*!
-  * Speedbump v1.0.1
+  * Speedbump v2.0.0
 */
 
-$.fn.speedBumpModal = function (options) {
+const speedBumpModal = function(options) {
+	const settings = Object.assign({
+		speedBumpTitle: 'You are leaving this website',
+		speedBumpSummary: 'You are leaving our website and entering a third party website over which we have no control. We do not endorse or guarantee and are not responsible for the content, links, privacy, or security of the website, or the products, services, information, or recommendations offered on this website.',
+		speedBumpContinueLink: 'Continue',
+		speedBumpContinueLinkClass: '',
+		speedBumpCancelLink: 'Cancel',
+		speedBumpCancelClass: '',
+		speedBumpIgnoreArray: [],
+		speedBumpHeadingTag: '<h4>',
+		speedBumpCloseButtonClass: '',
+		speedBumpCloseIconClass: '',
+	}, options);
 
-    // Pass array through settings
-    var settings = $.extend({
-        'speedBumpTitle': 'You are leaving this website',
-        'speedBumpSummary': 'You are leaving our website and entering a third party website over which we have no control. We do not endorse or guarantee and are not responsible for the content, links, privacy, or security of the website, or the products, services, information, or recommendations offered on this website.',
-        'speedBumpContinueLink': 'Continue',
-        'speedBumpContinueLinkClass': '',
-        'speedBumpCancelLink': 'Cancel',
-        'speedBumpCancelClass': '',
-        'speedBumpIgnoreArray': [],
-        'speedBumpHeadingTag': '<h4>',
-        'speedBumpCloseButtonClass': '',
-        'speedBumpCloseIconClass': ''
-    }, options);
-
-    var continueClass = (settings.speedBumpContinueLinkClass !== '') ? settings.speedBumpContinueLinkClass : '';
-    var cancelClass = (settings.speedBumpCancelClass !== '') ? settings.speedBumpCancelClass : '';
-    var closeClass = (settings.speedBumpCloseButtonClass !== '') ? settings.speedBumpCloseButtonClass : '';
-
+	const continueClass = settings.speedBumpContinueLinkClass !== '' ? settings.speedBumpContinueLinkClass : '';
+	const cancelClass = settings.speedBumpCancelClass !== '' ? settings.speedBumpCancelClass : '';
+	const closeClass = settings.speedBumpCloseButtonClass !== '' ? settings.speedBumpCloseButtonClass : '';
+	
     // Build outer speedbump modal div
-    $(document.createElement('div'))
-        .addClass('speed-bump')
-        .attr({
-            'tabindex': '-1',
-            'role': 'dialog',
-            'aria-modal': true,
-            'aria-labelledby': 'speedBumpTitle'
-        })
-        .appendTo('body');
-
+	const speedBumpModal = document.createElement('div');
+	speedBumpModal.classList.add('speed-bump');
+	speedBumpModal.tabIndex = '-1';
+	speedBumpModal.role = 'dialog';
+	speedBumpModal.setAttribute('aria-modal', true);
+	speedBumpModal.setAttribute('aria-labelledby', 'speedBumpTitle');
+	document.body.appendChild(speedBumpModal);
+	
     // Build outer speed-bump dialog
-    $(document.createElement('div'))
-        .addClass('speed-bump__dialog')
-        .attr('role', 'document')
-        .appendTo('.speed-bump');
-
+	const speedBumpDialog = document.createElement('div');
+	speedBumpDialog.classList.add('speed-bump__dialog');
+	speedBumpDialog.setAttribute('role', 'document');
+	speedBumpModal.appendChild(speedBumpDialog);
+	
     // Build speed-bump content
-    $(document.createElement('div'))
-        .addClass('speed-bump__content')
-        .appendTo('.speed-bump__dialog');
-
+	const speedBumpContent = document.createElement('div');
+	speedBumpContent.classList.add('speed-bump__content');
+	speedBumpDialog.appendChild(speedBumpContent);
+	
     // Build speed-bump title
-    $(settings.speedBumpHeadingTag)
-        .addClass('speed-bump__title')
-        .attr('id', 'speedBumpTitle')
-        .html(settings.speedBumpTitle)
-        .appendTo('.speed-bump__content');
-
+    const speedBumpHeadingTagString = settings.speedBumpHeadingTag.replace('<', '').replace('>', '');
+	const speedBumpTitle = document.createElement(speedBumpHeadingTagString);
+	speedBumpTitle.classList.add('speed-bump__title');
+	speedBumpTitle.id = 'speedBumpTitle';
+	speedBumpTitle.innerHTML = settings.speedBumpTitle;
+	speedBumpContent.appendChild(speedBumpTitle);
+	
     // Build speed-bump summary
-    $(document.createElement('p'))
-        .addClass('speed-bump__summary')
-        .html(settings.speedBumpSummary)
-        .appendTo('.speed-bump__content');
-
+	const speedBumpSummary = document.createElement('p');
+	speedBumpSummary.classList.add('speed-bump__summary');
+	speedBumpSummary.innerHTML = settings.speedBumpSummary;
+	speedBumpContent.appendChild(speedBumpSummary);
+	
     // Build speed-bump continue link
-    $(document.createElement('a'))
-        .addClass('speed-bump__continue')
-        .addClass(continueClass)
-        .attr('href', 'null')
-        .html(settings.speedBumpContinueLink)
-        .appendTo('.speed-bump__content');
-
+	const speedBumpContinueLink = document.createElement('a');
+	speedBumpContinueLink.classList.add('speed-bump__continue');
+    classesToArray(continueClass).forEach(item => {
+        speedBumpContinueLink.classList.add(item);
+    });
+	speedBumpContinueLink.href = 'null';
+	speedBumpContinueLink.innerHTML = settings.speedBumpContinueLink;
+	speedBumpContent.appendChild(speedBumpContinueLink);
+	
     // Build speed-bump cancel link
-    $(document.createElement('button'))
-        .addClass('speed-bump__cancel')
-        .addClass(cancelClass)
-        .attr({
-            'type': 'button'
-        })
-        .html(settings.speedBumpCancelLink)
-        .appendTo('.speed-bump__content');
-
+	const speedBumpCancelLink = document.createElement('button');
+	speedBumpCancelLink.classList.add('speed-bump__cancel');
+    classesToArray(cancelClass).forEach(item => {
+        speedBumpCancelLink.classList.add(item);
+    });
+    speedBumpCancelLink.type = 'button';
+	speedBumpCancelLink.innerHTML = settings.speedBumpCancelLink;
+	speedBumpContent.appendChild(speedBumpCancelLink);
+	
     // Build speed-bump close button
-    $(document.createElement('button'))
-        .addClass('speed-bump__close')
-        .addClass(closeClass)
-        .attr('type', 'button')
-        .attr('aria-label', 'Close')
-        .insertAfter('.speed-bump__title');
-
+	const speedBumpCloseButton = document.createElement('button');
+	speedBumpCloseButton.classList.add('speed-bump__close');
+    classesToArray(closeClass).forEach(item => {
+        speedBumpCloseButton.classList.add(item);
+    });
+	speedBumpCloseButton.setAttribute('type', 'button');
+	speedBumpCloseButton.setAttribute('aria-label', 'Close');
+	speedBumpCloseButton.setAttribute('type', 'button');
+	speedBumpContent.prepend(speedBumpCloseButton);
+	
     // Build speed-bump cancel button icon
-    if (settings.speedBumpCloseIconClass !== '') {
-        $(document.createElement('span'))
-            .addClass(settings.speedBumpCloseIconClass)
-            .attr('aria-hidden', 'true')
-            .appendTo('.speed-bump__close');
-    }
-
+	if (settings.speedBumpCloseIconClass !== '') {
+		const speedBumpCloseIcon = document.createElement('span');
+		speedBumpCloseIcon.className = settings.speedBumpCloseIconClass;
+		speedBumpCloseIcon.setAttribute('aria-hidden', 'true');
+		speedBumpCloseButton.appendChild(speedBumpCloseIcon);
+	}
+	
     // Trap the keyboard to the off canvas elements when speedbump modal is showing
-    var focusBeforeSpeedbump;
-    var trapKeyboardToSpeedbump = function () {
+	let focusBeforeSpeedbump;
+	const trapKeyboardToSpeedbump = () => {
+		focusBeforeSpeedbump = document.activeElement;
+		
+        const firstTabbable = document.querySelector('.speed-bump__close');
+		const lastTabbable = document.querySelector('.speed-bump__cancel');
+		firstTabbable.focus();
+		
+        lastTabbable.addEventListener('keydown', (e) => {
+			if (e.which === 9 && !e.shiftKey) {
+				e.preventDefault();
+				firstTabbable.focus();
+			}
+		});
 
-        // Cache anchor that fired speedbump
-        focusBeforeSpeedbump = $(':focus');
+		firstTabbable.addEventListener('keydown', (e) => {
+			if (e.which === 9 && e.shiftKey) {
+				e.preventDefault();
+				lastTabbable.focus();
+			}
+		});
 
-        var firstTabbable = $('.speed-bump__close');
-        var lastTabbable = $('.speed-bump__cancel');
+		document.querySelector('.speed-bump__content').focus();
+	};
 
-        // Set focus on first input
-        firstTabbable.focus();
+    // Hide modal
+	function speedBumpHide() {
+		document.body.classList.remove('js-speedbump-showing');
+		const anchor = document.querySelector('[data-anchor-active]');
+		if (anchor) {
+			anchor.focus();
+			anchor.removeAttribute('data-anchor-active');
+		}
+		document.querySelector('.speed-bump__continue').setAttribute('target', '');
+		focusBeforeSpeedbump.focus();
+	}
 
-        // Redirect last tab to first input
-        lastTabbable.on('keydown', function (e) {
-            if (e.which === 9 && !e.shiftKey) {
-                e.preventDefault();
-                firstTabbable.focus();
-            }
-        });
-
-        // Redirect first shift+tab to last input
-        firstTabbable.on('keydown', function (e) {
-            if (e.which === 9 && e.shiftKey) {
-                e.preventDefault();
-                lastTabbable.focus();
-            }
-        });
-
-        // Focus on the off canvas element
-        $('.speed-bump__content').focus();
-    };
-
-    // Close speed-bump function
-    function speedBumpHide() {
-        $('body').removeClass('js-speedbump-showing');
-        $('[data-anchor-active]')
-            .focus()
-            .removeAttr('data-anchor-active');
-        $('.speed-bump__continue').attr('target', '');
-        focusBeforeSpeedbump.focus();
+    // Convert class(es) string to array
+    function classesToArray(string) {
+        return string.toString().split(' ').join(',').split(',');
     }
 
-    // Setup array of domains to ignore
-    var flaggedArr = settings.speedBumpIgnoreArray;
-    var localUrl = location.host;
-    flaggedArr.push(localUrl);
+	const flaggedArr = settings.speedBumpIgnoreArray;
+	const localUrl = location.host;
+	flaggedArr.push(localUrl);
+	const cleanArray = flaggedArr.map((n) => n.split(' ').join('')).filter((arrayItem) => arrayItem !== '').map((arrayItem) => arrayItem.toLowerCase());
 
-    var cleanArray = $.map(flaggedArr, function (n, i){
-        var arrayItem = n.split(' ').join('');
-        
-        if (arrayItem === '') {
-            return;
-        } else {
-            return arrayItem.toLowerCase();
-        }
-    });
+	function absolutePath(href) {
+		const link = document.createElement('a');
+		link.href = href;
+		return link.protocol + '//' + link.host + link.pathname + link.search + link.hash;
+	}
 
-    function absolutePath(href) {
-        var link = document.createElement('a');
-        link.href = href;
-        return (link.protocol + '//' + link.host + link.pathname + link.search + link.hash);
-    }
+	document.querySelectorAll('a[href]').forEach((link) => {
+		const anchorHref = absolutePath(link.getAttribute('href').toLowerCase());
+		for (let i = cleanArray.length - 1; i >= 0; --i) {
+			if (anchorHref.indexOf(cleanArray[i]) !== -1) {
+				link.classList.add('js-no-speed-bump');
+			}
+		}
+	});
 
-    // Set class on internal and ignored links
-    $('a[href]').each(function () {
-        var $this = $(this),
-            anchorHref = absolutePath($this.attr('href').toLowerCase());
-
-        for (var i = cleanArray.length - 1; i >= 0; --i) {
-            if (anchorHref.indexOf(cleanArray[i]) !== -1) {
-                $this.addClass('js-no-speed-bump');
-            }
-        }
-    });
-
-    var selectorString = 
+	const externalLinkSelectorString =
         'a:not(.js-no-speed-bump)' +
         ':not([href^="#"])' +
         ':not([href^="/"])' +
         ':not(.speed-bump__continue)' +
         ':not([href=""])'
     ;
+	
+    document.querySelectorAll(externalLinkSelectorString).forEach((link) => {
+		link.addEventListener('click', (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			
+            document.body.classList.add('js-speedbump-showing');
+			
+            const currentUrl = link.getAttribute('href');
+			const newWindow = link.getAttribute('target') === '_blank';
+			
+            link.setAttribute('data-anchor-active', 'true');
+			
+            speedBumpModal.focus();
+			
+            document.querySelector('.speed-bump__continue').setAttribute('href', currentUrl);
+			
+            if (newWindow) {
+				document.querySelector('.speed-bump__continue').setAttribute('target', '_blank');
+			}
 
-    // Anchor click modal functionality
-    $(selectorString).on('click', function (e){
-        
-        $('body').addClass('js-speedbump-showing');
+			trapKeyboardToSpeedbump();
+		});
+	});
 
-        e.preventDefault();
-        e.stopPropagation();
+	const cancelButtons = document.querySelectorAll('.speed-bump__cancel, .speed-bump__close');
+    cancelButtons.forEach((button) => {
+		button.addEventListener('click', speedBumpHide);
+	});
 
-        var $this = $(this);
-        var $thisUrl = $this.attr('href');
-        var newWindow = ($this.attr('target') === '_blank') ? true : false;
-
-        // Add data attribute to currently active anchor
-        $this.attr('data-anchor-active', 'true');
-
-        // Display and focus on the speed-bump modal
-        $('.speed-bump')
-            .focus();
-
-        // Speedbump continue link
-        $('.speed-bump__continue').attr('href', $thisUrl);
-
-        if (newWindow === true) {
-            $('.speed-bump__continue').attr('target', '_blank');
-        }
-
-        trapKeyboardToSpeedbump();
-    });
-
-    // Speedbump cancel button click
-    $('.speed-bump__close, .speed-bump__cancel').on('click', function (e) {
-        speedBumpHide();
-    });
-
-    // Close speed bump after clicking continue
-    $('.speed-bump__continue').on('click', function () {
-        setTimeout(function (){
-            speedBumpHide();
-        }, 300);
-    });
-
-    // Click off speed-bump closes it
-    $('.speed-bump').on('click', function (e) {
-        if (!$(e.target).closest('.speed-bump__content').length) {
-            speedBumpHide();
-        }
-    }).on('keydown', function (e) {
-        if (e.keyCode === 27) {
-            speedBumpHide();
-        }
-    });
+	const continueButton = document.querySelector('.speed-bump__continue');
+	continueButton.addEventListener('click', () => {
+		setTimeout(speedBumpHide, 300);
+	});
+	
+    const speedBumpModalEl = document.querySelector('.speed-bump');
+	speedBumpModalEl.addEventListener('click', (e) => {
+		if (!e.target.closest('.speed-bump__content')) {
+			speedBumpHide();
+		}
+	});
+    
+	speedBumpModalEl.addEventListener('keydown', (e) => {
+		if (e.keyCode === 27) {
+			speedBumpHide();
+		}
+	});
 };
 //# sourceMappingURL=speedbump.js.map
